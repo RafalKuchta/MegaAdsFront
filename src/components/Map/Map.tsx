f1e265e55e582f0f8a43efafb18ed34caaf749c2
Rafa Kuchta <rafal.kuchta1@gmail.com>
2022-05-19 10:49:33 +0200

    Map and markers are finished.

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, {useContext, useEffect, useState} from 'react';
+import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import '../../utils/fix-map-icon';
+import {SearchContext} from "../../contexts/search.context";
+import {SimpleAdEntity} from 'types';
+
 import './Map.css';
+import {SingleAd} from "./SingleAd";
 
 export const Map = () => {
+    const {search} = useContext(SearchContext);
+    const [ads, setAds] = useState<SimpleAdEntity[]>([]);
+
+    useEffect(() => {
+        (async () => {
+
+            const res = await fetch(`http://localhost:3001/ad/search/${search}`);
+            const data = await res.json();
+
+            setAds(data);
+
+        })();
+    }, [search]);
+
+
     return (
-        <div className='map'>MAPA</div>
+        <div className='map'>
+            <MapContainer center={[52.3781118564998, 21.00736561259326]} zoom={20} scrollWheelZoom={false}>
+                <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution="&copy; <a href='https://www.openstreetmap.org/copyright'>OpenStreetMap</a> contributors"
+                />
+
+                {
+                    ads.map(ad => (
+                        <Marker key={ad.id} position={[ad.lat, ad.lon]}>
+                            <Popup>
+                                <SingleAd id={ad.id}/>
+                            </Popup>
+                        </Marker>
+                    ))
+                }
+            </MapContainer>
+        </div>
     )
 }
